9712fdc59cb5b0999cd1e5990e31a1eac56756e9
igor_qa <afanasyevigor@mail.ru>
2025-11-24 09:59:17 +0400

    init project

---
+++ src/main/java/core/base/BaseTest.java
@@ -0,0 +1,12 @@
+package core.base;
+
+import com.codeborne.selenide.Configuration;
+
+public class BaseTest extends AbstractBaseTest{
+
+    @Override
+    protected void configure() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+    }
+}
