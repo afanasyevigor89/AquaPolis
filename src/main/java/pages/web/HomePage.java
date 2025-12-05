23626f23bafd01c4c3d42648b7fc90d310ca6d7f
igor_qa <afanasyevigor@mail.ru>
2025-12-05 16:17:47 +0400

    add tests

--- src/main/java/pages/web/HomePage.java
+++ src/main/java/pages/web/HomePage.java
@@ -5,16 +5,19 @@ import com.codeborne.selenide.SelenideElement;
 import core.base.BasePage;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.cssClass;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.DragAndDropOptions.to;
 import static com.codeborne.selenide.Selenide.*;
 
 public class HomePage extends BasePage {
     protected SelenideElement bannerHomePage = $(".flicking-viewport.app-banner__flicking");
     protected SelenideElement promoCategoryHomePage = $(".app-index-page__categories-list");
-    protected SelenideElement buttonAddProductToCart = $x("(//button[contains(@class, 'app-cart-add-item-btn')])[1]");
+    protected SelenideElement buttonAddProductToCart = $x("(//button[.//span[text()='В корзину']])[1]");
     protected ElementsCollection paginationBullet = $$(".flicking-pagination-bullet");
+    protected SelenideElement addProductNotif = $("div[role='alert']");
+    protected SelenideElement countProductInCart = $("a[href='/checkout-spa'] .v-badge__badge span");
 
     @Step("Добавление товара в корзину")
     public void addProductToCart() {
@@ -23,12 +26,24 @@ public class HomePage extends BasePage {
 
     @Step("Пролистывание баннера свайпом")
     public void swipeBanner() {
-        actions().dragAndDropBy(bannerHomePage, 100, 0).perform(); // свайп вправо на 100px
+        bannerHomePage.shouldBe(visible);
+        actions().dragAndDropBy(bannerHomePage, 50, 0).perform(); // свайп вправо на 100px
     }
 
     @Step("Проверка активной точки пагинации баннера")
     public boolean verifyBulletIsActive (int indexBullet) {
-        paginationBullet.get(indexBullet).shouldHave(cssClass("flicking-pagination-bullet-active"));
-        return true;
+        return paginationBullet.get(indexBullet).shouldHave(cssClass("flicking-pagination-bullet-active")).exists();
+    }
+
+    @Step("Проверка изменения кол-ва товаров в корзине")
+    public boolean verifyCountProductInHeader(int count) {
+        return countProductInCart.shouldBe(visible)
+                .shouldHave(text(String.valueOf(count)))
+                .exists();
+    }
+
+    @Step("Получение нотифа после добавления товара в корзину")
+    public String getMessageText() {
+        return addProductNotif.should(exist, Duration.ofSeconds(5)).getText();
     }
 }
--- src/test/java/web/HomePageTests.java
+++ src/test/java/web/HomePageTests.java
@@ -2,34 +2,60 @@ package web;
 
 import core.base.BasePage;
 import core.base.BaseTest;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import pages.web.CategoryPage;
 import pages.web.HomePage;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HomePageTests extends BaseTest {
     private static HomePage homePage;
+    private static BasePage basePage;
 
     @BeforeEach
     public void prepared() {
-        BasePage basePage = new BasePage();
         homePage = new HomePage();
 
         open(baseUrl);
 
-        basePage.allowCookie();
+        basePage = new BasePage();
+        basePage.allowCookie();// закрываем плашку с cookie
+
     }
     @Test
+    @DisplayName("TC-003: Прокручивание слайдера с баннерами свайпом")
+    @Feature("Главная страница")
+    @Story("Пользователь может пролистать баннер")
+    @Severity(SeverityLevel.NORMAL)
     public void swipeHomePageBannerTest() {
 
         homePage.swipeBanner();
         //Проверяем что активна 3-я точка пагинации
-        assertTrue(homePage.verifyBulletIsActive(2));
+        assertTrue(homePage.verifyBulletIsActive(2), "Баннер слайдера не прокрутился");
     }
 
+    @Test
+    @DisplayName("TC-004: Добавление товара в корзину из слайдера Новинки")
+    @Feature("Главная страница")
+    @Story("Пользователь может добавить товар в корзину из слайдера Новинки")
+    @Severity(SeverityLevel.CRITICAL)
+    public void addProductToCartFromHomePage(){
+        executeJavaScript("window.scrollTo(0, 600)");
+        homePage.addProductToCart();
 
+        //Проверяем что появился нотиф добавления товара в корзину
+        String expectMessage = "Товар добавлен в корзину";
+        String actualMessage = homePage.getMessageText();
+        assertEquals(expectMessage, actualMessage, "Текст сообщения не совпадает");
+
+        //Проверяем что изменился счетчик товаров в хэдере
+        assertTrue(homePage.verifyCountProductInHeader(1), "Кол-во товаров в хэдере указано неверно");
+    }
 }
